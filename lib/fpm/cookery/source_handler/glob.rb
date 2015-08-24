a85bc632b92400e3ed7fae26aeb829caae83554d
Brian OConnell <boc@us.ibm.com>
2015-08-23 22:16:16 -0400

    Added extract method to glob.

--- lib/fpm/cookery/source_handler/glob.rb
+++ lib/fpm/cookery/source_handler/glob.rb
@@ -1,22 +1,45 @@
 require 'fpm/cookery/source_handler/template'
 require 'fpm/cookery/log'
 require 'fileutils'
+require 'pry'
 
 module FPM
   module Cookery
     class SourceHandler
       class Glob < FPM::Cookery::SourceHandler::Template
         CHECKSUM = false
-        NAME = :local_path
+        NAME = :glob
 
         def fetch(config = {})
-          if local_path.exist?
-            Log.info "Using cached file #{local_path}"
-          else
-            Log.info "Copying #{source.path} to cache"
-            FileUtils.cp_r(source.path, cachedir)
+          Dir.glob(source.path).each do |file| 
+            cached_file = File.join(@cachedir,file)
+            if File.exist? cached_file
+              Log.info "Using cached file #{cached_file}"
+            else
+              Log.info "Copying #{file} to cache"
+              FileUtils.cp_r(file, cachedir)
+            end
           end
           local_path
+        end
+
+        def extract(config = {})
+          Dir.chdir(builddir) do
+            Dir.glob(local_path).each do |file| 
+	    case File.extname file
+            when '.bz2', '.gz', '.tgz', '.xz'
+              safesystem('tar', 'xf', file)
+            when '.shar', '.bin'
+              File.chmod(0755, file)
+              safesystem(file)
+            when '.zip'
+              safesystem('unzip', '-d', file.basename(file,'.zip'), file)
+            else
+              FileUtils.cp_r(file, builddir)  
+            end
+          end
+          builddir
+          end
         end
       end
     end
